d87a33e75040e25b132db3a46892c89bdde3f0cc
ekkaikaa <76554662+ekkaikaa@users.noreply.github.com>
2021-10-04 14:59:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('mobileNet', modelloaded)
+}
+
+function draw() {
+  image(video, 0,0, 300,300 )
+  classifier.classify(video,gotresults)
+}
+
+function modelloaded() {
+console.log("Model has been loaded succesfully"); 
+}
+
+function gotresults(error, results) {
+if (error) {
+  console.log(error)
+} 
+else{
+console.log(results)
+document.getElementById("result_object_name").innerHTML = resutls[0].label
+document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3)
+}}
